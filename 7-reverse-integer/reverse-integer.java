6056a974351faddfb205853eafdb2feaf6b7d5e2
Harsh Gupta <152484145+harshgupta565@users.noreply.github.com>
2025-07-02 23:29:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (85.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,39 +1,14 @@
-// class Solution {
-//     public int reverse(int x) {
-//         int revnum = 0;
-//         int length = (x == 0) ? 1 : (int)Math.log10(Math.abs(x)) + 1;
-//         int count = 0;
-//         int m =Integer.MAX_VALUE;
-//         int n = Integer.MIN_VALUE;
-//          if(x  > m && x < n){
-//             return 0;}
-//         while(count < length ){
-//         int digit = x%10;
-//          revnum = (revnum*10)+digit;
-//         x=x/10;
-//         count ++;
-
-        
-       
-//         }
-//         return revnum;
-
-//     }
 class Solution {
     public int reverse(int x) {
-        int rev = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-
-            // Check for overflow before doing rev * 10
-            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
-            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
-
-            rev = rev * 10 + digit;
-            x /= 10;
+        int  revsnum = 0;
+        while(x!=0){
+            int d = x % 10;
+            if( (revsnum> Integer.MAX_VALUE/10) || (revsnum<Integer.MIN_VALUE/10)){
+        return 0;
         }
-
-        return rev;
-    }
-}
+            revsnum = (revsnum*10)+d;
+            x = x/10;
+        }
+            return revsnum;
+         }
+       }
