56c34bd604870e510e9aa2d8aaf9a02279f469eb
Harsh Gupta <152484145+harshgupta565@users.noreply.github.com>
2025-07-05 19:51:02 +0530

    Time: 16 ms (15.67%) | Memory: 45.5 MB (13.61%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        // isse na sbb character htt jayege bss a  to b  ot 0 to 9 tkk ke chodke 
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         System.out.println(s); 
 
         int n = s.length();
-        for(int i = 0 ; i< n ; i++){
+        for(int i = 0 ; i< n/2 ; i++){
             if(s.charAt(i) != s.charAt (n-i-1))
             return false ;
         }
