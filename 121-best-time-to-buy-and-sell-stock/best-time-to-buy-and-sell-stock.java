ccad1516120fa28ee6807f409b7e0075055eb9dd
Harsh Gupta <152484145+harshgupta565@users.noreply.github.com>
2025-07-08 19:21:59 +0530

    Time: 1 ms (99.87%) | Memory: 61.4 MB (88.88%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,19 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int maxprofit = 0;
-        int minprice = Integer.MAX_VALUE;
+        int minpurchase = Integer.MAX_VALUE;
         for(int price : prices){
-            if(price < minprice){
-                minprice = price;
-            }else if(price - minprice>maxprofit){
-                maxprofit = price-minprice;
+            if(minpurchase > price){
+                minpurchase = price;
             }
+            else if (price - minpurchase > maxprofit){
+                maxprofit =  price - minpurchase ;
+
+            }
+
+
         }
-        return maxprofit;
+        return maxprofit ; 
+
     }
 }
