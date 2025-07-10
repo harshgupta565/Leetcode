979de8a6542b7de92da9be529027e506909a4eaf
Harsh Gupta <152484145+harshgupta565@users.noreply.github.com>
2025-07-11 01:30:25 +0530

    Time: 5 ms (31.25%) | Memory: 44.3 MB (71.35%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,10 +1,19 @@
 class Solution {
     public String intToRoman(int num) {
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hundereds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String thousands[]={"","M","MM","MMM"};
-        
-        return thousands[num/1000] + hundereds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String sym[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String ans="";
+        for(int x=0;x<values.length;x++)
+        {
+            if(num==0)break;
+            while(num >=values[x])
+            {
+                ans+=sym[x];
+                num-=values[x];
+            }
+        }
+        return ans ; 
+
+
     }
 }
