dc553079bb318aaef74ace179a5fa76170158719
Harsh Gupta <152484145+harshgupta565@users.noreply.github.com>
2025-08-04 23:53:07 +0530

    Time: 2 ms (55.71%) | Memory: 46.5 MB (12.16%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
-            }
-
-            if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
-            } else {
-                new_arr[k++] = nums2[j++];
-            }
+        int m = nums1.length;
+        int n =nums2.length;
+        int [] arr = new int [m+n];
+        int j = 0;
+        int i = 0;
+        int k = 0;
+        
+        while(i<m && j<n){
+            // agr nums1 chhoti hh to temp wali apni array me apn nums1[i] value wali index ko daal denge 
+        if(nums1[i] < nums2[j]){
+            arr[k]=nums1[i];
+            k++;
+            i++;
         }
-
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
-    }
+        else{
+            arr[k]=nums2[j];
+            k++;
+            j++;
+        }
+        }
+        while(i<m){
+        arr[k]= nums1[i];
+        i++;
+        k++;
+        }
+        while(j<n){
+            arr[k] = nums2[j];
+            j++;k++;
+        }
+        
+        int l = m+n;
+        if(l%2 == 1){
+            return arr[l/2];
+          }
+        else{
+            return(arr[l/2]+arr[(l/2)-1])/2.0;
+        }
+        }
 }
